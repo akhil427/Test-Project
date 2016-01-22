eda4a8da374cd8d88fac0a18d886a864a71c6d6d
akhil427 <akhilchowdary427@gmail.com>
2016-01-21 21:58:35 -0500

    learning branch changes

---
+++ array.java
@@ -0,0 +1,14 @@
+
+public class array {
+
+			public static void main(String args[]) {
+			int x[] = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+			for (int i = 0; i < x.length; i++)
+			System.out.print(x[i] + "\t");
+			}
+			
+		// TODO Auto-generated method stub
+
+	}
+
+
